fb8e73502d558f75cd668c83da5f68f1f6227c1f
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 14:19:51 +0100

    Update sv_framework.lua

--- lua/vector-moderation/sv_framework.lua
+++ lua/vector-moderation/sv_framework.lua
@@ -4,5 +4,5 @@ AddCSLuaFile("vm_framework.lua")
 hook.Add("PlayerSay", "vm_HandleCommands", function(Player, Message, TeamChat)
   if (TeamChat) then return end
   local Args = string.Explode(" ", Message)
-  return vm:HandleCommands(Player, Args)
+  return vm.HandleCommands(Player, Args)
 end)
