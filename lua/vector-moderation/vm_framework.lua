0b4d7ca6aed4231e2f49307b0700a48234c53870
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 12:02:16 +0100

    Update vm_framework.lua

--- lua/vector-moderation/vm_framework.lua
+++ lua/vector-moderation/vm_framework.lua
@@ -4,7 +4,7 @@
 vm.ConsoleMessage("Loading dependancies")
 
 include(vm_util.lua)
-vm.IncludeFolder( "vm_util" )
+vm.IncludeFolder("vm_util")
 
 -- Handle variables --
 vm.Version = version_util.Version( 0, 0, 1 )
@@ -13,27 +13,6 @@ vm.Version = version_util.Version( 0, 0, 1 )
 vm.ConfigDefaults = {}
 vm.ConfigDefaults["AutoUpdate"] = false
 
--- Handle config --
-vm.ConsoleMessage("Loading config")
-vm.Config = vm.GetData("Config") or {}
-
-if vm.Config == {} then
-    vm.ConsoleMessage("No config found, creating defaults")
-end
-
-vm.DefaultConfig()
-
-function vm.DefaultConfig()
-    for a, b in pairs(vm.ConfigDefaults) do
-        if vm.Config[a] == nil then
-            vm.Config[a] = vm.ConfigDefaults[a]
-        elseif type(vm.Config[a]) != type(vm.ConfigDefaults[a]) then
-            vm.ConsoleMessage("Config value " .. a .. " with value " .. vm.Config[a] .. " is not the correct type, reverting to default")
-            vm.Config[a] = vm.ConfigDefaults[a]
-        end
-    end
-end
-
-function vm:GetConfig( Key )
-    return vm.Config[Key] or vm.ConsoleMessage("Attempted to find config value " .. key .. " but found none, please report this error to the developers")
-end
+-- Handle addons --
+vm.Addons = {}
+vm.IncludeFolder("vm_addons")
