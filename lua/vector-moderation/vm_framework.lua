a523d9dd5462d69cdd50973af80c8ff0044f5119
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 14:15:04 +0100

    Update vm_framework.lua

--- lua/vector-moderation/vm_framework.lua
+++ lua/vector-moderation/vm_framework.lua
@@ -16,26 +16,52 @@ vm.ConfigDefaults["AutoUpdate"] = false
 -- Handle addons --
 vm.Addons = {}
 vm.IncludeFolder("vm_addons")
+concommand.Add("vm", function(Player, Command, Args)
+	
+	local ValidCommands = {}
+	
+	for a, b in pairs(vm.Addons) do
+		if (!b["Commands"]) then continue end
+		for c, d in pairs(b.Commands) do
+			if (Command:lower == d.Name:lower()) then
+				ValidCommands:insert(d)
+			elseif (d["Aliases"]) then
+				for e in d["Aliases"] do
+					if (Command:lower() == e:lower() then
+						table.insert(ValidCommands, d)
+					end
+				end
+			end
+		end
+	end
+	
+	if (ValidCommands == {}) then
+		return
+	elseif (#ValidCommands > 1) then
+		Player:PrintMessage( HUD_PRINTTALK, "Multiple commands found using that alias" )
+		return ""
+	end
+	
+	ValidCommands[1]:Run(Player, Args)
+end
 
 function vm.RegisterAddon( Addon )
 	table.insert(vm.Addons, Addon)
 	vm.ConsoleMessage("Loaded " .. Addon.Name .. " as an addon")
 end
 
-function vm.HandleCommands( Player, Message )
+function vm.HandleCommands( Player, Args )
 	local ValidCommands = {}
-	local Args = vm:SeperateWords(Message:sub(2))
-	if (#Args == 0) then return end
 	
 	for a, b in pairs(vm.Addons) do
 		if (!b["Commands"]) then continue end
 		for c, d in pairs(b.Commands) do
-			if (Message:sub(1, 1):lower() == d.Prefix:lower()) then
-				if (Args[1]:lower == d.Name:lower()) then
+			if (Args[1]:sub(1, 1):lower() == d.Prefix:lower()) then
+				if (Args[1]:sub(2):lower == d.Name:lower()) then
 					ValidCommands:insert(d)
 				elseif (d["Aliases"]) then
 					for e in d["Aliases"] do
-						if (Args[1]:lower() == e:lower() then
+						if (Args[1]:sub(2):lower() == e:lower() then
 							table.insert(ValidCommands, d)
 						end
 					end
@@ -51,5 +77,5 @@ function vm.HandleCommands( Player, Message )
 		return ""
 	end
 	
-	ValidCommands[1]:Run(Player, Message:sub(1 + Args[1]:len())
+	ValidCommands[1]:Run(Player, table.remove(Args, 1))
 end
