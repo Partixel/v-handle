0e54fa62a51f63fff197d4a6789af3a4b5228498
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 14:00:53 +0100

    Update vm_framework.lua

--- lua/vector-moderation/vm_framework.lua
+++ lua/vector-moderation/vm_framework.lua
@@ -21,3 +21,36 @@ function vm.RegisterAddon( Addon )
 	table.insert(vm.Addons, Addon)
 	vm.ConsoleMessage("Loaded " .. Addon.Name .. " as an addon")
 end
+
+function vm.HandleCommands( Player, Message )
+	local ValidCommands = {}
+	local Args = {}
+	for a in Message:sub(2):gmatch("%S+") do
+	    table.insert(Args, a)
+	end
+	for a, b in pairs(vm.Addons) do
+		if (!b["Commands"]) then continue end
+		for c, d in pairs(b.Commands) do
+			if (Message:sub(1, 1):lower() == d.Prefix:lower()) then
+				if (Args[1]:lower == d.Name:lower()) then
+					ValidCommands:insert(d)
+				elseif (d["Aliases"]) then
+					for e in d["Aliases"] do
+						if (Args[1]:lower() == e:lower() then
+							table.insert(ValidCommands, d)
+						end
+					end
+				end
+			end
+		end
+	end
+	
+	if (ValidCommands == {}) then
+		return
+	elseif (#ValidCommands > 1) then
+		Player:PrintMessage( HUD_PRINTTALK, "Multiple commands found using that alias" )
+		return ""
+	end
+	
+	ValidCommands[1]:Run(Player, Message:sub(1 + Args[1]:len())
+end
