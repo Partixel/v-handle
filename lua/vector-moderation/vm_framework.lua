e500f9311ee7aed55fcb00c52ca7ef11fbefc77b
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 15:21:12 +0100

    Update vm_framework.lua

--- lua/vector-moderation/vm_framework.lua
+++ lua/vector-moderation/vm_framework.lua
@@ -23,7 +23,7 @@ concommand.Add("vm", function(Player, Command, Args)
 	for a, b in pairs(vm.Addons) do
 		if (!b["Commands"]) then continue end
 		for c, d in pairs(b.Commands) do
-			if (Command:lower == d.Name:lower()) then
+			if (Command:lower() == d.Name:lower()) then
 				table.insert(ValidCommands, d)
 			elseif (d["Aliases"]) then
 				for e in d["Aliases"] do
@@ -63,7 +63,7 @@ function vm.HandleCommands( Player, Args )
 		for c, d in pairs(b.Commands) do
 			if (d.Prefix == "") then continue end
 			if (Args[1]:sub(1, 1):lower() == d.Prefix:lower()) then
-				if (Args[1]:sub(2):lower == d.Name:lower()) then
+				if (Args[1]:sub(2):lower() == d.Name:lower()) then
 					table.insert(ValidCommands, d)
 				elseif (d["Aliases"]) then
 					for e in d["Aliases"] do
