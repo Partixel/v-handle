f3c0ebc97f9dad7685e093b4bc898fb429760cf5
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-10 15:16:19 +0100

    Update Vector-Moderation.lua

--- lua/autorun/Vector-Moderation.lua
+++ lua/autorun/Vector-Moderation.lua
@@ -2,4 +2,8 @@ vm = vm or {}
 
 if (SERVER) then
 	include("vector-moderation/sv_framework.lua")
+	include("vector-moderation/vm_framework.lua")
+	AddCSLuaFile("vector-moderation/vm_framework.lua")
+else
+	include("vector-moderation/vm_framework.lua")
 end
