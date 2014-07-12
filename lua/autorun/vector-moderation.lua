013ea474d011c93cd1519dbc32afd329b73878cd
EliteJynx <EliteJynx@yahoo.co.uk>
2014-07-12 22:59:23 +0100

    Update and rename Vector-Moderation.lua to vector-moderation.lua

--- lua/autorun/vector-moderation.lua
+++ lua/autorun/vector-moderation.lua
@@ -3,7 +3,6 @@ vm = vm or {}
 if (SERVER) then
 	include("vector-moderation/sv_framework.lua")
 	include("vector-moderation/vm_framework.lua")
-	AddCSLuaFile("vector-moderation/vm_util.lua")
 	AddCSLuaFile("vector-moderation/vm_framework.lua")
 else
 	include("vector-moderation/vm_framework.lua")
