d3fff44f73055ec590c575090568688207e7c5d8
Partixel <EliteJynx@yahoo.co.uk>
2014-07-24 23:37:47 +0100

    Documented the use of modules

--- lua/v-handle/vh_modules/sh_defaultmodule.lua
+++ lua/v-handle/vh_modules/sh_defaultmodule.lua
@@ -1,19 +1,23 @@
-local Module = {}
-Module.Name = "Default"
-Module.Description = "Default Module"
-Module.Commands = {}
-Module.Commands.DefaultCommand = {
-  Aliases = {"AnotherCommand"},
-  Prefix = "!",
-  Description = "Does default stuff",
-  Usage = "<Player>"
+local Module = {} -- A list containing the modules values and functions
+Module.Name = "Default" -- Name of the module
+Module.Description = "Default Module" -- Description of the module
+Module.Commands = {} -- List of commands this module adds
+Module.Commands.DefaultCommand = { -- A command
+  Aliases = {"AnotherCommand"}, -- Names that can be used to run the command as well as the key
+  Prefix = "!", -- The character(s) that needs to be put before the command
+  Description = "Does default stuff", -- The desciption of the command for display
+  Usage = "<Player>" -- A description of how the command is used
 }
 
-function Module.Commands.DefaultCommand.Run(Player, Args)
+function Module.Commands.DefaultCommand.Run(Player, Args) -- The function that is ran via the framework when the command is ran
 	Player:PrintMessage( HUD_PRINTTALK, "Ran default command!" )
 end
 
-function Module.Commands.DefaultCommand.Vars(ArgNumber)
+function Module.Commands.DefaultCommand.Vars(ArgNumber) -- The function that is called when getting valid inputs for arguements
+							-- return nil if the #Arguement is invalid
+							-- ArgNumber is the position of the arguement;
+							-- e.g. !cloak part 1 - "part" would be arguement 1 and in this
+							-- function would return a list of player names
 	if (ArgNumber == 1) then
 		return {"1", "2"}
 	elseif (ArgNumber == 2) then
@@ -22,11 +26,21 @@ function Module.Commands.DefaultCommand.Vars(ArgNumber)
 	return
 end
 
-Module.ConCommands = {}
-Module.ConCommands.DefaultConCommand = {}
+Module.ConCommands = {} -- list of console commands this module adds ( Seperate to vh_[command] )
+Module.ConCommands.DefaultConCommand = {} -- A console command, the key is what is registered as a console command
 
-function Module.ConCommands.DefaultConCommand.Run(Player, Command, Args)
+function Module.ConCommands.DefaultConCommand.Run(Player, Command, Args) -- The function that is ran via the framework when the
+									 -- console command is ran
 	Player:PrintMessage( HUD_PRINTTALK, "Ran default command!" )
 end
 
-vh.RegisterModule(Module)
+Module.Hooks = { -- List of hooks required by the module
+	{
+		Type = "PlayerSay", -- The hook required
+		Run = (function( Player, Message, TeamChat ) -- The function to be ran with the corresponding variables
+			Player:PrintMessage( HUD_PRINTTALK, "Hook triggered!" )	
+		end)
+	}
+}
+
+vh.RegisterModule(Module) -- Registers this module to the framework for handling of commands, console commands and hooks
