d80286d4dcab5e66f269437781ffb3e48c869def
Partixel <EliteJynx@yahoo.co.uk>
2014-09-04 20:30:32 +0100

    Updated to more universal namings and ...
    descriptions of log types
    Only writes to file if it is between MessageLevel and LogLevel

--- lua/v-handle/libs/V-LogLib/main.lua
+++ lua/v-handle/libs/V-LogLib/main.lua
@@ -6,13 +6,15 @@ _V.LogLib.LogLocation = "_V/Logs" -- The directory the file is located in
 _V.LogLib.LogFile = "log" -- The name of the file - %D gets replaced by the date
 
 _V.LogLib.Type = {
-	Message = 0, -- Used for messages to the console
-	Error = 1, -- Used to report a fatal error
-	Warning = 2 -- Used to warn of a potential/recoverable error
+	INFO = 0, -- Used for informational messages
+	SEVERE = 1, -- Used to indicate a serious failure
+	WARNING = 2, -- Used to indicate a potential problem
+	DEBUG = 3, -- Used to supply additional debug informational messages
+	CONFIG = 4, -- Used to supply static config variables
 }
 
-_V.LogLib.MessageLevel = 3 -- Any log type lower or equal to this will be printed to console
-_V.LogLib.LogLevel = 1 -- Any log type higher or equal to this will be written to file
+_V.LogLib.MessageLevel = 2 -- Any log type lower or equal to this will be printed to console
+_V.LogLib.LogLevel = 1 -- Any log type between or equal to this and the MessageLevel will be written to file
 
 if !file.Exists(_V.LogLib.LogLocation .. "/" .. _V.LogLib.LogFile, "DATA") then
 	file.CreateDir(_V.LogLib.LogLocation) -- Make sure the directories exist
@@ -22,12 +24,16 @@ function _V.LogLib.Log(Message, Type)
 	if type(Message) == "table" then
 		
 	elseif type(Message) == "string" then
-		if Type == _V.LogLib.Type.Message then
+		if Type == _V.LogLib.Type.INFO then
 			Message = "V-Handle - " .. Message
-		elseif Type == _V.LogLib.Type.Warning then
+		elseif Type == _V.LogLib.Type.WARNING then
 			Message = "V-Handle - WARNING - " .. Message
-		elseif Type == _V.LogLib.Type.Error then
+		elseif Type == _V.LogLib.Type.SEVERE then
 			Message = "V-Handle - ERROR - " .. Message
+		elseif Type == _V.LogLib.Type.DEBUG then
+			Message = "V-Handle - DEBUG - " .. Message
+		elseif Type == _V.LogLib.Type.CONFIG then
+			Message = "V-Handle - CONFIG - " .. Message
 		else
 			_V.LogLib.Log("Invalid log type type: ".. Message, _V.LogLib.Type.Error)
 			return
@@ -36,7 +42,7 @@ function _V.LogLib.Log(Message, Type)
 		if Type <= _V.LogLib.MessageLevel then
 			print(Message) -- Print the message to the console
 		end
-		if Type >= _V.LogLib.LogLevel then
+		if _V.LogLib.LogLevel <= Type and Type <= _V.LogLib.MessageLevel then
 			local CurrentFile = tostring(os.date("%d.%m.%y-" .. _V.LogLib.LogFile .. ".txt"))
 			file.Append(_V.LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
 			-- Add the message to the log file
