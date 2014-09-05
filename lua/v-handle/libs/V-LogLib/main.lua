ef74633590ba8c319c1ac8da5807b501069fa77a
Partixel <EliteJynx@yahoo.co.uk>
2014-09-05 12:53:18 +0100

    Re-ordered variables
    Config vars are first now

--- lua/v-handle/libs/V-LogLib/main.lua
+++ lua/v-handle/libs/V-LogLib/main.lua
@@ -5,6 +5,9 @@ _V.LogLib = {}
 _V.LogLib.LogLocation = "_V/Logs" -- The directory the file is located in
 _V.LogLib.LogFile = "log" -- The name of the file - %D gets replaced by the date
 
+_V.LogLib.MessageLevel = 2 -- Any log type lower or equal to this will be printed to console
+_V.LogLib.LogLevel = 1 -- Any log type between or equal to this and the MessageLevel will be written to file
+
 _V.LogLib.Type = {
 	INFO = 0, -- Used for informational messages
 	SEVERE = 1, -- Used to indicate a serious failure
@@ -12,9 +15,6 @@ _V.LogLib.Type = {
 	DEBUG = 3, -- Used to supply additional debug informational messages
 	CONFIG = 4, -- Used to supply static config variables
 }
-
-_V.LogLib.MessageLevel = 2 -- Any log type lower or equal to this will be printed to console
-_V.LogLib.LogLevel = 1 -- Any log type between or equal to this and the MessageLevel will be written to file
 
 if !file.Exists(_V.LogLib.LogLocation .. "/" .. _V.LogLib.LogFile, "DATA") then
 	file.CreateDir(_V.LogLib.LogLocation) -- Make sure the directories exist
