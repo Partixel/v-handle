366e7b0f62f6b8e0e5601f0d89ce3f5304cccdc9
Nick Lock <EliteJynx@yahoo.co.uk>
2014-09-05 18:02:45 +0100

    Finishes the ConfigLib
    Adds categories for the config values
    Adds descriptions for config values and categories
    Adds correctly formatted file saving/writing for readable configs
    NEXT:
    Make configvalues get saved per category

--- lua/v-handle/libs/V-ConfigLib/main.lua
+++ lua/v-handle/libs/V-ConfigLib/main.lua
@@ -10,42 +10,143 @@ file.CreateDir(_V.ConfigLib.ConfigLocation) -- Make sure the directories exist
 _V.ConfigLib.Containers = {}
 -- A table containing a cache of the configs to save on file I/O
 
+_V.ConfigLib.Categories = {
+	TEST = 0 -- The category; Number relative to the position of it's object in
+			 -- _V.ConfigLib.CategoryObjects
+}
+
+_V.ConfigLib.CategoryObjects = {
+	[0] = {
+		Name = "Test", -- Name of the category used for display
+		Desc = "For the test categories!" -- Description of the category for display
+	}
+}
+
+_V.ConfigLib.ConfigValues = {}
+-- A list of ConfigValue objects for use in writing to file
+
+_V.ConfigLib.ConfigValue = {
+	Key = nil, -- Key of the value
+	Container = nil, -- Container file of the value
+	Default = nil, -- Default value
+	Desc = nil, -- Description of the value
+	Category = nil -- Category the value is under
+}
+
+_V.ConfigLib.CorrectValues = {
+	number = function(Value) return tonumber(Value) end,
+	-- Used to convert from string to number
+	boolean = function(Value) return string.lower(Value) == "true" end
+	-- Used to convert from string to boolean
+}
+
+function _V.ConfigLib.ConfigValue:Get() -- Returns the current value of the ConfigValues key
+	return _V.ConfigLib.Get(self.Key, self.Container, self.Default, self)
+end
+
+function _V.ConfigLib.ConfigValue:ToString(Value) -- Returns the ConfigValue as a nicely formatted string
+	local Text = self.Key .. " = "
+	Text = Text .. type(Value) .. " = " .. tostring(Value)
+	if self.Desc then
+		Text = Text .. " --" .. self.Desc
+	end
+	return Text
+end
+
+function _V.ConfigLib.ConfigValue:FromString(String) -- Returns the key and value of a ConfigValues string
+	local SplitA = string.Explode(" --", String)
+	if SplitA and SplitA[1] then
+		local SplitB = string.Explode(" = ", SplitA[1])
+		if SplitB and SplitB[1] and SplitB[2] and SplitB[3] then
+			-- Gets the key [1], value type [2] and value [3]
+			local Value = _V.ConfigLib.CorrectValue(SplitB[3], SplitB[2])
+			return {Key = SplitB[1], Value = Value}
+		end
+	end
+end
+
+function _V.ConfigLib.ConfigValue:new(Key, Container, Default, Desc, Category)
+	-- Makes a new config value and initializes it. USE THIS FOR NEW CONFIGVALUES
+	-- Desc and category are option
+	-- Category should be the number relative to the CategoryObjects position within the table
+	local Object = {Key = Key, Container = Container, Default = Default, Desc = Desc, Category = Category}
+	setmetatable(Object, self)
+	self.__index = self
+	Object:Get() -- Initializes the object
+	_V.ConfigLib.ConfigValues[Key] = Object
+	return Object
+end
+
+function _V.ConfigLib.CorrectValue(Value, Type)
+	-- Corrects a value to the type specified
+	local FixedValue = Value
+	if _V.ConfigLib.CorrectValues[Type] then
+		FixedValue = _V.ConfigLib.CorrectValues[Type](Value)
+	end
+	return FixedValue
+end
+
 function _V.ConfigLib.ReadConfig(Location)
 	-- Reads the config from file and formats it correctly
 	local Data = file.Read(_V.ConfigLib.ConfigLocation .. "/" .. Location .. ".txt", "DATA")
 	if Data and Data != "" then
-		return von.deserialize(util.Decompress(Data))
+		local ConfigValues = {}
+		Data = string.Explode("\n",Data)
+		for a, b in pairs(Data) do
+			if b == "" then continue end
+			local ConfigValue = _V.ConfigLib.ConfigValue:FromString(b)
+			if ConfigValue then
+				ConfigValues[ConfigValue.Key] = ConfigValue.Value
+			end
+		end
+		return ConfigValues
 	end
 end
 
 function _V.ConfigLib.WriteConfig(Location, Data)
 	-- Writes the config to file and formats it correctly
-	file.Write(_V.ConfigLib.ConfigLocation .. "/" .. Location .. ".txt", util.Compress(von.serialize(Data)))
+	local Formatted = ""
+	
+	for a, b in pairs(Data) do
+		local ConfigVal = _V.ConfigLib.ConfigValues[a]
+		if ConfigVal then
+			Formatted = Formatted .. ConfigVal:ToString(b) .. "\n"
+		else
+			Formatted = Formatted .. a .. " = " .. type(b) .. " = " .. b .. " --No valid ConfigValue found\n"
+		end
+	end
+	file.Write(_V.ConfigLib.ConfigLocation .. "/" .. Location .. ".txt", Formatted)
 end
 
-function _V.ConfigLib.Get(Key, ConKey, Default)
+function _V.ConfigLib.ReloadConfig()
+	-- Will effectively reload the config from file, requires the script to have already been loaded
+	_V.ConfigLib.Containers = {}
+	for a, b in pairs(_V.ConfigLib.ConfigValues) do
+		b:Get()
+	end
+end
+
+function _V.ConfigLib.Get(Key, ConKey, Default, ConfigValue)
 	-- Returns the value of the key (KEY) within the specificied config (CONKEY)
 	-- If value or config file is not found it returns default and saves file
+	if Key == nil or ConKey == nil or Default == nil then return end
+	-- Nil check
+	
 	local Container = _V.ConfigLib.Containers[ConKey]
 	if Container != nil then -- Check if the config is cached
 		if Container[Key] then
 			if type(Container[Key]) != type(Default) then
 				-- Make sure the data is valid
 				Container[Key] = Default
+				_V.ConfigLib.WriteConfig(ConKey, Container)
+				-- Save the default value
 			end
 			return Container[Key]
 		else
-			local Data = _V.ConfigLib.ReadConfig(ConKey)
-			if Data[Key] and type(Data[Key]) == type(Default) then
-				-- Make sure the data is valid
-				Container[Key] = Data[Key]
-				return Container[Key]
-			else
-				Container[Key] = Default
-				_V.ConfigLib.WriteConfig(ConKey, Container)
-				-- Save the corrected value
-				return Default
-			end
+			Container[Key] = Default
+			_V.ConfigLib.WriteConfig(ConKey, Container)
+			-- Save the default value
+			return Default
 		end
 	else
 		local Data = _V.ConfigLib.ReadConfig(ConKey)
--- lua/v-handle/libs/V-LogLib/main.lua
+++ lua/v-handle/libs/V-LogLib/main.lua
@@ -1,15 +1,12 @@
 _V = _V or {}
-
 _V.LogLib = {}
-
-_V.LogLib.LogLocation = _V.ConfigLib.Get("LogLocation", "LibConfig", "_V/Logs")
+_V.LogLib.LogLocation = _V.ConfigLib.ConfigValue:new("LogLocation", "LibConfig", "_V/Logs", "Desc!", _V.ConfigLib.Categories.TEST)
 -- The directory the file is located in
-_V.LogLib.LogFile = _V.ConfigLib.Get("LogFile", "LibConfig", "log")
+_V.LogLib.LogFile = _V.ConfigLib.ConfigValue:new("LogFile", "LibConfig", "log")
 -- The name of the file - %D gets replaced by the date
-
-_V.LogLib.MessageLevel = _V.ConfigLib.Get("MessageLevel", "LibConfig", 2)
+_V.LogLib.MessageLevel = _V.ConfigLib.ConfigValue:new("MessageLevel", "LibConfig", 4)
 -- Any log type lower or equal to this will be printed to console
-_V.LogLib.LogLevel = _V.ConfigLib.Get("LogLevel", "LibConfig", 1)
+_V.LogLib.LogLevel = _V.ConfigLib.ConfigValue:new("LogLevel", "LibConfig", 1)
 -- Any log type between or equal to this and the MessageLevel will be written to file
 
 _V.LogLib.Type = {
@@ -20,12 +17,13 @@ _V.LogLib.Type = {
 	CONFIG = 4, -- Used to supply static config variables
 }
 
-file.CreateDir(_V.LogLib.LogLocation) -- Make sure the directories exist
+file.CreateDir(_V.LogLib.LogLocation:Get()) -- Make sure the directories exist and initializes LogLocation
 
 function _V.LogLib.Log(Message, Type)
 	if type(Message) == "table" then
 		
 	elseif Message != nil then
+		Message = tostring(Message)
 		if Type == _V.LogLib.Type.INFO then
 			Message = "V-Handle - " .. Message
 		elseif Type == _V.LogLib.Type.WARNING then
@@ -41,29 +39,31 @@ function _V.LogLib.Log(Message, Type)
 			return
 		end
 		-- Formates the messages depending on the type
-		
-		if Type <= _V.LogLib.MessageLevel then
+		local MessageLevel = _V.LogLib.MessageLevel:Get()
+		-- Allows for only one :Get() call
+		if Type <= MessageLevel then
 			-- Checks it the message should be printed to console
 			print(Message) -- Print the message to the console
 		end
-		if _V.LogLib.LogLevel <= Type and Type <= _V.LogLib.MessageLevel then
+		
+		if _V.LogLib.LogLevel:Get() <= Type and Type <= MessageLevel then
 			-- Checks if the message should be logged to file
-			local CurrentFile = tostring(os.date("%d.%m.%y-" .. _V.LogLib.LogFile .. ".txt"))
+			local CurrentFile = tostring(os.date("%d.%m.%y-" .. _V.LogLib.LogFile:Get() .. ".txt"))
 			-- Creates/finds the file for the current day
 			Message = os.date("%X ") .. string.Explode(" ", os.date("%Z"))[1] .. " - " .. Message
 			-- Formats time and time zone into the message for log purposes
-			file.Append(_V.LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
+			file.Append(_V.LogLib.LogLocation:Get() .. "/" .. CurrentFile, Message .. "\n")
 			-- Add the message to the log file
 		end
 	else
-		_V.LogLib.Log("Invalid message type: ".. Message, _V.LogLib.Type.SEVERE)
+		_V.LogLib.Log("Invalid message type: ".. type(Message), _V.LogLib.Type.SEVERE)
 		return
 	end
 end
 
 -- DEBUG MESSAGES
 
-_V.LogLib.Log(_V.LogLib.LogLocation, _V.LogLib.Type.CONFIG)
-_V.LogLib.Log(_V.LogLib.LogFile, _V.LogLib.Type.CONFIG)
-_V.LogLib.Log(_V.LogLib.MessageLevel, _V.LogLib.Type.CONFIG)
-_V.LogLib.Log(_V.LogLib.LogLevel, _V.LogLib.Type.CONFIG)
+_V.LogLib.Log(_V.LogLib.LogLocation:Get(), _V.LogLib.Type.CONFIG)
+_V.LogLib.Log(_V.LogLib.LogFile:Get(), _V.LogLib.Type.CONFIG)
+_V.LogLib.Log(_V.LogLib.MessageLevel:Get(), _V.LogLib.Type.CONFIG)
+_V.LogLib.Log(_V.LogLib.LogLevel:Get(), _V.LogLib.Type.CONFIG)
