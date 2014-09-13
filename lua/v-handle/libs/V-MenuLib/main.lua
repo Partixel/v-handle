75228463677732dd9f49fecbdc91daa9ec2a5d3a
CodeNil <codednil@yahoo.co.uk>
2014-09-13 14:51:24 +0100

    Redo alot of how menus work

--- lua/v-handle/libs/V-MenuLib/main.lua
+++ lua/v-handle/libs/V-MenuLib/main.lua
@@ -1,27 +1,26 @@
 _V.MenuLib = {}
 
 local RegisteredTabs = {}
-local OpenTabs = {}
 local ActiveTab = 1
 local TabsVisible = false
 
-_V.MenuLib.Settings = {}
-_V.MenuLib.Settings.Sizes = {}
-_V.MenuLib.Settings.Sizes.Main = {x = 0.45, y = 0.6}
-_V.MenuLib.Settings.Colors = {}
-_V.MenuLib.Settings.Colors.Active = Color(245, 245, 245, 255)
-_V.MenuLib.Settings.Colors.Inactive = Color(215, 215, 215, 255)
-_V.MenuLib.Settings.Colors.Main = Color(245, 245, 245, 255)
-_V.MenuLib.Settings.Colors.MainButton = Color(225, 225, 225, 255)
-_V.MenuLib.Settings.Colors.Secondary = Color(30, 140, 160, 255)
-_V.MenuLib.Settings.Colors.SecondaryButton = Color(60, 170, 190, 255)
-_V.MenuLib.Settings.Colors.Tertiary = Color(160, 30, 30, 255)
-_V.MenuLib.Settings.Colors.TertiaryButton = Color(190, 60, 60, 255)
-_V.MenuLib.Settings.Colors.Text = Color(50, 50, 50, 255)
-_V.MenuLib.Settings.Textures = {}
-_V.MenuLib.Settings.Textures.Normal = Material("vgui/white")
-_V.MenuLib.Settings.Textures.GradientDown = Material("gui/gradient_down.png")
-_V.MenuLib.Settings.Textures.GradientUp = Material("gui/gradient_up.png")
+Settings = {}
+Settings.Sizes = {}
+Settings.Sizes.Main = {x = 0.45, y = 0.6}
+Settings.Colors = {}
+Settings.Colors.Active = Color(245, 245, 245, 255)
+Settings.Colors.Inactive = Color(215, 215, 215, 255)
+Settings.Colors.Main = Color(245, 245, 245, 255)
+Settings.Colors.MainButton = Color(225, 225, 225, 255)
+Settings.Colors.Secondary = Color(30, 140, 160, 255)
+Settings.Colors.SecondaryButton = Color(60, 170, 190, 255)
+Settings.Colors.Tertiary = Color(160, 30, 30, 255)
+Settings.Colors.TertiaryButton = Color(190, 60, 60, 255)
+Settings.Colors.Text = Color(50, 50, 50, 255)
+Settings.Textures = {}
+Settings.Textures.Normal = Material("vgui/white")
+Settings.Textures.GradientDown = Material("gui/gradient_down.png")
+Settings.Textures.GradientUp = Material("gui/gradient_up.png")
 
 if CLIENT then
 	surface.CreateFont("VHUIFontLarge", {
@@ -77,35 +76,56 @@ end
 _V.MenuLib.VTab = {
 	Name = nil, 		-- Name of the Tab
 	RenderCall = nil, 	-- Function to call on render
-	Closable = nil, 	-- Can the tab be closed
 	Position = nil 		-- Forced position of the tab
 }
 
-function _V.MenuLib.VTab:new(Name, RenderCall, Closable, Position)
-	local Object = {Name = Name, RenderCall = RenderCall, Closable = Closable, Position = Position}
-	if Object.Closable == nil then
-		Object.Closable = true
-	end
+function _V.MenuLib.VTab:new(Name, RenderCall, Position)
+	local Object = {Name = Name, RenderCall = RenderCall, Position = Position}
 	setmetatable(Object, self)
 	self.__index = self
 	_V.MenuLib.RegisterTab(Object)
 	return Object
 end
 
+function _V.MenuLib.GetRegisteredTabs()
+	return RegisteredTabs
+end
+
+function _V.MenuLib.RedrawTabs(OldActiveTab)
+	DrawTabs(OldActiveTab)
+end
+
+function _V.MenuLib.SetActiveTab(NewActiveTab)
+	ActiveTab = NewActiveTab
+end
+
+function _V.MenuLib.GetActiveTab()
+	return ActiveTab
+end
+
+function _V.MenuLib.GetSettings()
+	return Settings
+end
+
 function _V.MenuLib.OpenTab(VTab)
-	_V.MenuLib.RegisterTab(VTab)
-	if not table.HasValue(OpenTabs, VTab) then
-		if VTab.Position then
-			table.insert(OpenTabs, VTab.Position, VTab)
-		else
-			table.insert(OpenTabs, VTab)
+	local Old = ActiveTab
+	ActiveTab = VTab.Position or #RegisteredTabs
+	for i, v in ipairs(RegisteredTabs) do
+		if v == VTab then
+			ActiveTab = i
 		end
 	end
+	DrawTabs(Old)
+	ShowMenu()
 end
 
 function _V.MenuLib.RegisterTab(VTab)
 	if not table.HasValue(RegisteredTabs, VTab) then
-		table.insert(RegisteredTabs, VTab)
+		if VTab.Position then
+			table.insert(RegisteredTabs, VTab.Position, VTab)
+		else
+			table.insert(RegisteredTabs, VTab)
+		end
 	end
 end
 
@@ -131,10 +151,10 @@ function _V.MenuLib.DrawTrapezoid( x, y, w, h, col, a)
 end
 
 function _V.MenuLib.DrawTrapezoidFancy( x, y, w, h, col, a, b)
-	surface.SetMaterial(_V.MenuLib.Settings.Textures.Normal)
+	surface.SetMaterial(Settings.Textures.Normal)
 	_V.MenuLib.DrawTrapezoid(b, b, w - b * 2, h - b * 2, col, a)
 	_V.MenuLib.DrawTrapezoid(0, 0, w, h, giveAlphaAlt(col), a)
-	surface.SetMaterial(_V.MenuLib.Settings.Textures.GradientUp)
+	surface.SetMaterial(Settings.Textures.GradientUp)
 	_V.MenuLib.DrawTrapezoid(b, b, w - b * 2, h - b * 2, Color(60, 60, 60, 100 * (col.a/255)), a)
 end
 
@@ -182,11 +202,11 @@ function DrawTabs(OldActiveTab)
 	end
 	GUI.Tabs = {}
 	GUI.Tabs.Active = {}
-	local ShownX = ScrW() * (1 - _V.MenuLib.Settings.Sizes.Main.x)/2
-	local ShownY = ScrH() * (1 - _V.MenuLib.Settings.Sizes.Main.y)/2
-	if #OpenTabs == 0 then return end
-	ActiveTab = math.max(ActiveTab, 1)
-	for i, v in pairs(OpenTabs) do
+	local ShownX = ScrW() * (1 - Settings.Sizes.Main.x)/2
+	local ShownY = ScrH() * (1 - Settings.Sizes.Main.y)/2
+	if #RegisteredTabs == 0 then return end
+	if SERVER then return end
+	for i, v in pairs(RegisteredTabs) do
 		local Active = i == ActiveTab
 		if not Active then
 			local Offset = math.abs(i - ActiveTab) - 1
@@ -196,53 +216,48 @@ function DrawTabs(OldActiveTab)
 				GUI.Tabs[i] = vgui.Create("DPanel", GUI.MasterFrame)
 				GUI.Tabs[i]:SetPos(ShownX + OffsetOld, ShownY - OffsetOld)
 				GUI.Tabs[i]:MoveTo(ShownX + OffsetNew, ShownY - OffsetNew, 0.5, 0, 1)
-				GUI.Tabs[i]:SetSize(ScrW() * _V.MenuLib.Settings.Sizes.Main.x, ScrH() * _V.MenuLib.Settings.Sizes.Main.y)
+				GUI.Tabs[i]:SetSize(ScrW() * Settings.Sizes.Main.x, ScrH() * Settings.Sizes.Main.y)
 				GUI.Tabs[i].Paint = function()
-					_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[i]:GetWide(), GUI.Tabs[i]:GetTall(), giveAlpha(_V.MenuLib.Settings.Colors.Inactive, Offset), 40, 5)
+					_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[i]:GetWide(), GUI.Tabs[i]:GetTall(), giveAlpha(Settings.Colors.Inactive, Offset), 40, 5)
 				end
 			end
 		end
 	end
-	local Offset = ((ActiveTab - 1) - (#OpenTabs/2)) * 40
+	local Offset = ((ActiveTab - 1) - (#RegisteredTabs/2)) * 40
 	local OffsetOld = (ActiveTab - (OldActiveTab or ActiveTab)) * 40
 	GUI.Tabs[ActiveTab] = vgui.Create("DPanel", GUI.MasterFrame)
 	GUI.Tabs[ActiveTab]:SetPos(ShownX + OffsetOld, ShownY - OffsetOld)
 	GUI.Tabs[ActiveTab]:MoveTo(ShownX, ShownY, 0.5, 0, 1)
-	GUI.Tabs[ActiveTab]:SetSize(ScrW() * _V.MenuLib.Settings.Sizes.Main.x, ScrH() * _V.MenuLib.Settings.Sizes.Main.y)
+	GUI.Tabs[ActiveTab]:SetSize(ScrW() * Settings.Sizes.Main.x, ScrH() * Settings.Sizes.Main.y)
 	GUI.Tabs[ActiveTab].Paint = function()
-		_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[ActiveTab]:GetWide(), GUI.Tabs[ActiveTab]:GetTall(), _V.MenuLib.Settings.Colors.Active, 40, 5)
+		_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[ActiveTab]:GetWide(), GUI.Tabs[ActiveTab]:GetTall(), Settings.Colors.Active, 40, 5)
 	end
 	GUI.Tabs[ActiveTab].Label = vgui.Create("DLabel", GUI.Tabs[ActiveTab])
 	GUI.Tabs[ActiveTab].Label:SetPos(0, 0)
 	GUI.Tabs[ActiveTab].Label:SetSize(GUI.Tabs[ActiveTab]:GetWide(), 40)
 	GUI.Tabs[ActiveTab].Label:SetText("")
 	GUI.Tabs[ActiveTab].Label.Paint = function()
-		draw.SimpleText(OpenTabs[ActiveTab].Name, "VHUIFont", GUI.Tabs[ActiveTab]:GetWide()/2, 20, Color(50, 50, 50, 255), 1, 1)
+		draw.SimpleText(RegisteredTabs[ActiveTab].Name, "VHUIFont", GUI.Tabs[ActiveTab]:GetWide()/2, 20, Color(50, 50, 50, 255), 1, 1)
 	end
-	if OpenTabs[ActiveTab].Closable then
-		GUI.Tabs[ActiveTab].CloseButton = vgui.Create("DButton", GUI.Tabs[ActiveTab])
-		GUI.Tabs[ActiveTab].CloseButton:SetPos(GUI.Tabs[ActiveTab]:GetWide() - 90, 10)
-		GUI.Tabs[ActiveTab].CloseButton:SetSize(80, 30)
-		GUI.Tabs[ActiveTab].CloseButton:SetText("")
-		GUI.Tabs[ActiveTab].CloseButton.Paint = function()
-			_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[ActiveTab].CloseButton:GetWide(), GUI.Tabs[ActiveTab].CloseButton:GetTall(), _V.MenuLib.Settings.Colors.TertiaryButton, 2, 2)
-			draw.SimpleText("x", "VHUIFont", GUI.Tabs[ActiveTab].CloseButton:GetWide()/2, GUI.Tabs[ActiveTab].CloseButton:GetTall()/2 - 4, Color(50, 50, 50, 255), 1, 1)
-		end
-		GUI.Tabs[ActiveTab].CloseButton.DoClick = function()
-			local Old = ActiveTab
-			table.remove(OpenTabs, ActiveTab)
-			ActiveTab = math.min(ActiveTab, #OpenTabs)
-			DrawTabs(Old)
-		end
+	GUI.Tabs[ActiveTab].CloseButton = vgui.Create("DButton", GUI.Tabs[ActiveTab])
+	GUI.Tabs[ActiveTab].CloseButton:SetPos(GUI.Tabs[ActiveTab]:GetWide() - 90, 10)
+	GUI.Tabs[ActiveTab].CloseButton:SetSize(80, 30)
+	GUI.Tabs[ActiveTab].CloseButton:SetText("")
+	GUI.Tabs[ActiveTab].CloseButton.Paint = function()
+		_V.MenuLib.DrawTrapezoidFancy(0, 0, GUI.Tabs[ActiveTab].CloseButton:GetWide(), GUI.Tabs[ActiveTab].CloseButton:GetTall(), Settings.Colors.TertiaryButton, 2, 2)
+		draw.SimpleText("x", "VHUIFont", GUI.Tabs[ActiveTab].CloseButton:GetWide()/2, GUI.Tabs[ActiveTab].CloseButton:GetTall()/2 - 4, Color(50, 50, 50, 255), 1, 1)
+	end
+	GUI.Tabs[ActiveTab].CloseButton.DoClick = function()
+		HideMenu()
 	end
 	GUI.Tabs[ActiveTab].Panel = vgui.Create("DPanel", GUI.Tabs[ActiveTab])
 	GUI.Tabs[ActiveTab].Panel:SetPos(50, 50)
 	GUI.Tabs[ActiveTab].Panel:SetSize(GUI.Tabs[ActiveTab]:GetWide() - 100, GUI.Tabs[ActiveTab]:GetTall() - 100)
 	GUI.Tabs[ActiveTab].Panel.Paint = function() end
-	if OpenTabs[ActiveTab].RenderCall then
-		OpenTabs[ActiveTab].RenderCall(GUI.Tabs[ActiveTab].Panel, GUI.Tabs.Active)
+	if RegisteredTabs[ActiveTab].RenderCall then
+		RegisteredTabs[ActiveTab].RenderCall(GUI.Tabs[ActiveTab].Panel, GUI.Tabs.Active)
 	end
-	for i, v in pairs(OpenTabs) do
+	for i, v in pairs(RegisteredTabs) do
 		local Active = i == ActiveTab
 		if not Active then
 			local Offset = math.abs(i - ActiveTab) - 1
@@ -256,13 +271,13 @@ function DrawTabs(OldActiveTab)
 					GUI.Tabs[i].Label:SetPos(ShownX + OffsetOld, ShownY - OffsetOld + GUI.Tabs[ActiveTab]:GetTall() - 40)
 					GUI.Tabs[i].Label:MoveTo(ShownX + OffsetNew, ShownY - OffsetNew + GUI.Tabs[ActiveTab]:GetTall() - 40, 0.5, 0, 1)
 					GUI.Tabs[i].Label.Paint = function()
-						draw.SimpleText(v.Name, "VHUIFontSmall", GUI.Tabs[ActiveTab]:GetWide()/2, 20, _V.MenuLib.Settings.Colors.Text, 1, 1)
+						draw.SimpleText(v.Name, "VHUIFontSmall", GUI.Tabs[ActiveTab]:GetWide()/2, 20, Settings.Colors.Text, 1, 1)
 					end
 				else
 					GUI.Tabs[i].Label:SetPos(ShownX + OffsetOld, ShownY - OffsetOld)
 					GUI.Tabs[i].Label:MoveTo(ShownX + OffsetNew, ShownY - OffsetNew, 0.5, 0, 1)
 					GUI.Tabs[i].Label.Paint = function()
-						draw.SimpleText(v.Name, "VHUIFontSmall", GUI.Tabs[ActiveTab]:GetWide()/2, 20, _V.MenuLib.Settings.Colors.Text, 1, 1)
+						draw.SimpleText(v.Name, "VHUIFontSmall", GUI.Tabs[ActiveTab]:GetWide()/2, 20, Settings.Colors.Text, 1, 1)
 					end
 				end
 				GUI.Tabs[i].Label.DoClick = function()
@@ -275,16 +290,33 @@ function DrawTabs(OldActiveTab)
 	end
 end
 
-concommand.Add("vh_menu", function() ShowMenu() end)
-concommand.Add("+vh_menu", function() ShowMenu() end)
-concommand.Add("-vh_menu", function() HideMenu() end)
+--[[function RenderCall(Frame, Table)
+	for i, v in pairs(_V.MenuLib.GetRegisteredTabs()) do
+		if v.Name ~= "Main" then
+			Table[v.Name] = vgui.Create("DButton", Frame)
+			Table[v.Name]:SetPos(Frame:GetWide() * 0.2 - (3 * (i - 1)), 10 + (35 * (i - 1)))
+			Table[v.Name]:SetSize(Frame:GetWide() * 0.6, 30)
+			Table[v.Name]:SetText("")
+			Table[v.Name].Paint = function()
+				_V.MenuLib.DrawTrapezoidFancy(0, 0, Table[v.Name]:GetWide(), Table[v.Name]:GetTall(), _V.MenuLib.GetSettings().Colors.MainButton, 2, 2)
+				draw.SimpleText(v.Name, "VHUIFontSmall", Table[v.Name]:GetWide()/2, Table[v.Name]:GetTall()/2 - 4, Color(50, 50, 50, 255), 1, 1)
+			end
+			Table[v.Name].DoClick = function()
+				_V.MenuLib.OpenTab(v)
+			end
+		end
+	end
+end
 
+local IndexTab = _V.MenuLib.VTab:new("Main", RenderCall, false, 1)
+
+_V.MenuLib.RegisteredTabsilent(IndexTab)]]
 
 local TestTab = _V.MenuLib.VTab:new("TestTab", nil)
 local TestTabB = _V.MenuLib.VTab:new("TestTabB", nil)
 local TestTabC = _V.MenuLib.VTab:new("TestTabC", nil)
 local TestTabD = _V.MenuLib.VTab:new("A Tab", nil)
-_V.MenuLib.OpenTab(TestTab)
-_V.MenuLib.OpenTab(TestTabB)
-_V.MenuLib.OpenTab(TestTabC)
-_V.MenuLib.OpenTab(TestTabD)
+
+concommand.Add("vh_menu", function() ShowMenu() end)
+concommand.Add("+vh_menu", function() ShowMenu() end)
+concommand.Add("-vh_menu", function() HideMenu() end)
