3eaa45a9a300222eacaa352e7941be25aaf97e61
Partixel <EliteJynx@yahoo.co.uk>
2014-09-13 15:14:45 +0100

    Runs file lib before-hand

--- lua/autorun/v-handle.lua
+++ lua/autorun/v-handle.lua
@@ -1,11 +1,14 @@
 vh = vh or {}
 
 if (SERVER) then
+	include("v-handle/libs/V-FileLib/main.lua")
 	include("v-handle/sv_main.lua")
 	include("v-handle/sh_main.lua")
+	AddCSLuaFile("v-handle/libs/V-FileLib/main.lua")
 	AddCSLuaFile("v-handle/sh_main.lua")
 	AddCSLuaFile("v-handle/cl_main.lua")
 else
+	include("v-handle/libs/V-FileLib/main.lua")
 	include("v-handle/sh_main.lua")
 	include("v-handle/cl_main.lua")
 end
