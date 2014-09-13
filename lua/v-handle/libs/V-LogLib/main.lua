84fac9c5215af5f632b476bffca52a34bada1a8e
Partixel <EliteJynx@yahoo.co.uk>
2014-09-13 18:40:53 +0100

    Update main.lua

--- lua/v-handle/libs/V-LogLib/main.lua
+++ lua/v-handle/libs/V-LogLib/main.lua
@@ -1,5 +1,7 @@
 _V = _V or {}
 
+_V.FileLib.RequireLib("ConfigLib")
+
 _V.LogLib = {}
 
 _V.LogLib.LogLocation = _V.ConfigLib.ConfigValue:new("LogLocation", "LibConfig", "_V/Logs", "Desc!", _V.ConfigLib.Categories.TEST)
