ad061c9bda9c46f5c646fac3d57976c269ff0083
Partixel <EliteJynx@yahoo.co.uk>
2014-09-13 15:09:34 +0100

    Finishes file lib

--- lua/v-handle/libs/V-FileLib/main.lua
+++ lua/v-handle/libs/V-FileLib/main.lua
@@ -35,7 +35,23 @@ function _V.FileLib.IncludeFile(Location, Side)
 end
 
 function _V.FileLib.IncludeDir(Dir)
-	
+	local Files, Dirs = file.Find("v-handle/" .. Dir .. "/*", "LUA")
+
+	for a, b in ipairs(Files) do
+		if string.lower(string.Right(b, 4)) == ".lua" then
+			if string.lower(string.Left(b, 3)) == "cl_" then
+				vh.IncludeFile(Name .. "/" .. b, _V.FileLib.Side.Client)
+			elseif string.lower(string.Left(b, 3)) == "sv_" then
+				vh.IncludeFile(Name .. "/" .. b, _V.FileLib.Side.Server)
+			else
+				vh.IncludeFile(Name .. "/" .. b, _V.FileLib.Side.Shared)
+			end
+		end
+	end
+
+	for a, b in ipairs(Dirs) do
+		vh.IncludeFolder(Name .. "/" .. b)
+	end
 end
 
 function _V.FileLib.Logger(Message, Type)
