00fc5c738da28a27f19bd9574e4c0f9159af6242
Nick Lock <EliteJynx@yahoo.co.uk>
2014-10-02 17:17:11 +0100

    Adds missed command functions
    Should be finished now and work

--- lua/v-handle/libs/V-CommandLib/main.lua
+++ lua/v-handle/libs/V-CommandLib/main.lua
@@ -2,46 +2,111 @@ _V = _V or {}
 
 _V.CommandLib = {}
 
+-- Arg handling --
+
 _V.CommandLib.BoolTrue = {"true", "y", "yes", "1"}
 _V.CommandLib.BoolFalse = {"false", "n", "no", "0"}
 
-_V.CommandLib.ArgTypes = {
-	TargetPlayer = function(String, Sender)
-		local Player = GETPLAYERHERE
-		if SENDERCANTARGETPLAYER then
-			return Player
+function _V.CommandLib.PlayerFromString(String)
+	if string.match(String, "STEAM_[0-5]:[0-9]:[0-9]+") then
+		return String
+	elseif String == "*" then
+		local Found = {}
+		for a, b in ipairs(player.GetAll()) do
+			table.insert(Found, player:SteamID())
 		end
-	end, -- A player that the sender can target
-	MultiTargetPlayer = function(String, Sender)
-		local Players = GETPLAYERSHERE
-		local TargetablePlayers = {}
-		for a, b in pairs(Players) do
-			if SENDERCANTARGETB then
-				table.insert(TargetablePlayers, b)
+		return Found
+	elseif String == "@" then
+		local Found = {}
+		for a, b in ipairs(player.GetAll()) do
+			if b:IsAdmin() then
+				table.insert(Found, b:SteamID())
 			end
 		end
-		return TargetablePlayers
-	end, -- A list of players that the sender can target
-	Player = function(String, Sender)
-		return GETPLAYERHERE
-	end, -- A player ( doesn't require targetability )
-	MultiPlayer = function(String, Sender)
-		return GETPLAYERSHERE
-	end, -- A list of players ( doesn't require targetability )
-	String = function(String)
+		return Found
+	elseif String == "!@" then
+		local Found = {}
+		for a, b in ipairs(player.GetAll()) do
+			if !b:IsAdmin() then
+				table.insert(Found, b:SteamID())
+			end
+		end
+		return Found
+	else
+		local Found = {}
+		for a, b in ipairs(player.GetAll()) do
+			if string.lower(b:Nick()) == string.lower(String) then
+				return {b:SteamID()}
+			elseif string.sub(string.lower(b:Nick()), 1, string.len(String)) == string.lower(String) then
+				table.insert(Found, b:SteamID())
+			end
+		end
+		return Found
+	end
+end
+
+function _V.CommandLib.PlayerFromSID(SID)
+	for a, b in ipairs(player.GetAll()) do
+		if b:SteamID() == SID then
+			return b
+		end
+	end
+end
+
+_V.CommandLib.ArgTypes = {
+	TargetPlayer = {Parser = function(String, Sender)
+		local Players = _V.CommandLib.PlayerFromString(String)
+		if #Players > 1 or #Players == 0 then
+			return "INC_TYPE"
+		end
+		--if SENDERCANTARGETPLAYER then
+			return Players[1]
+		--end
+	end, Name = "Targetable Player (Singular)"}, -- A player that the sender can target
+	MultiTargetPlayer = {Parser = function(String, Sender)
+		local Players = _V.CommandLib.PlayerFromString(String)
+		local Targets = {}
+		for a, b in pairs(Players) do
+			--if SENDERCANTARGETB then
+				table.insert(Targets, b)
+			--end
+		end
+		if #Targets == 0 then
+			return "INC_TYPE"
+		end
+		return Targets
+	end, Name = "Targetable Players (Multiple)"}, -- A list of players that the sender can target
+	Player = {Parser = function(String, Sender)
+		local Players = _V.CommandLib.PlayerFromString(String)
+		if #Players > 1 or #Players == 0 then
+			return "INC_TYPE"
+		end
+		return Players[1]
+	end, Name = "Player (Singular)"}, -- A player ( doesn't require targetability )
+	MultiPlayer = {Parser = function(String, Sender)
+		local Players = _V.CommandLib.PlayerFromString(String)
+		if #Players == 0 then
+			return "INC_TYPE"
+		end
+		return Players
+	end, Name = "Players (Multiple)"}, -- A list of players ( doesn't require targetability )
+	String = {Parser = function(String)
 		return String
-	end, -- A string
-	Number = function(String)
-		return tonumber(String)
-	end, -- A number
-	Boolean = function(String)
+	end, Name = "String"}, -- A string
+	Number = {Parser = function(String)
+		return tonumber(String) or "INC_TYPE"
+	end, Name = "Number"}, -- A number
+	Boolean = {Parser = function(String)
 		if table.HasValue(_V.CommandLib.BoolTrue, string.lower(String)) then
 			return true
 		elseif stable.HasValue(_V.CommandLib.BoolFalse, string.lower(String)) then
 			return false
 		end
-	end, -- Check if the string is contained in either the Yes or No table
+		return "INC_TYPE"
+	end, Name = "Boolean"}, -- Check if the string is contained in either the Yes or No table
 }
+
+-- Command handling --
 
 _V.CommandLib.Commands = {}
 
@@ -76,13 +141,14 @@ end
 
 function _V.CommandLib.Command:addArg(ArgType, ArgRequirement, Position)
 	-- Adds the specified arg type and requirement into the commands args at the specified position
-	table.insert(self.Args, Position, {type = ArgType, required = ArgRequirement})
+	local Arg = table.Copy(ArgType)
+	Arg.required = ArgRequirement or true
+	table.insert(self.Args, Position, Arg)
 	return self
 end
 
 function _V.CommandLib.Command:preCall(Sender, Args, teamChat)
 	-- This is called by the playersay hook to handle args and alias
-	-- Can be overriden if the command requires something different
 	
 	-- Makes sure the command isn't said in teamchat
 	if teamChat then
@@ -91,7 +157,7 @@ function _V.CommandLib.Command:preCall(Sender, Args, teamChat)
 
 	-- Check if the args contain the commands alias
 	local AliasUsed = ""
-	for a, b in pairs(self.Alias) do
+	for a, b in ipairs(self.Alias) do
 		if string.lower(Args[1]) == string.lower(b) do
 			AliasUsed = b
 			break
@@ -108,20 +174,25 @@ function _V.CommandLib.Command:preCall(Sender, Args, teamChat)
 		return ""
 	end
 	
+	--[[
 	-- Make sure the sender can run the command
 	if not Sender:HASPERM(self.Key) then
 		// ERROR INCORRECT PERMS
 		return ""
 	end
+	--]]
 	
 	-- Convert the args into the correct types
 	local FinalArgs = {}
 	for a, b in ipairs(self.Args) do
-		local Arg = b.type(Args[a], Sender)
-		if Arg then
+		local Arg = b.Parser(Args[a], Sender)
+		if Arg != nil and Arg != "INC_TYPE" then
 			table.insert(FinalArgs, Arg)
+		elseif Arg == "INC_TYPE" then
+			// "Arguement " .. a .. " was incorrect!" .. usage(?)
+			return ""
 		elseif b.required then
-			// ERROR MISSING ARG
+			// "Arguement " .. a .. " was missing" .. usage(?)
 			return ""
 		else
 			table.insert(FinalArgs, "nil")
@@ -132,12 +203,33 @@ function _V.CommandLib.Command:preCall(Sender, Args, teamChat)
 	return self.Callback(Sender, AliasUsed, unpackNil(FinalArgs))
 end
 
-function _V.CommandLib.Command:new(Key, Callback, Category, Desc)
-	-- Creates the new command with they Key and Callback (Callback can be provided later)
+function _V.CommandLib.Command:getUsage(Alias, ArgPos)
+	-- Returns the usage of the command or the type of a specific arguement
+	-- Alias specifies the alias to be used and ArgPos specifies the arguement type to use
+	if ArgPos then
+		return self.Args[ArgPos]
+	else
+		local Alias = Alias or self.Alias[1]
+		local Usage = Alias
+		for a, b in ipairs(self.Args) do
+			if b.required then
+				Usage = Usage .. " " .. b.Name
+			else
+				Usage = Usage .. " [" .. b.Name .. "]"
+			end
+		end
+1		return Usage
+	end
+end
+
+function _V.CommandLib.Command:new(Key, Callback, Category, Desc, ...)
+	-- Creates a new command object and adds it to the command list
+	-- Requires key to be valid
 	local Object = {Key = Key, Callback = Callback, Category = Category, Desc = Desc}
 	setmetatable(Object, self)
 	self.__index = self
 	table.insert(_V.CommandLib.Commands, Object)
+	Object:addAlias(unpack(arg))
 	return Object
 end
 
