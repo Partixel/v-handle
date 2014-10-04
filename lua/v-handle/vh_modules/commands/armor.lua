9d7f060082e728c42cbfc981d01a4e7e17325e27
CodeNil <codednil@yahoo.co.uk>
2014-10-04 12:42:16 +0100

    New arg for armor based on the ingame name for it

--- lua/v-handle/vh_modules/commands/armor.lua
+++ lua/v-handle/vh_modules/commands/armor.lua
@@ -1,7 +1,7 @@
 local Command = _V.CommandLib.Command:new("Armor", _V.CommandLib.UserTypes.Admin, "Sets the player(s) armor to specified amount or 100.", "")
 Command:addArg(_V.CommandLib.ArgTypes.Players, {required = false})
 Command:addArg(_V.CommandLib.ArgTypes.Number, {required = false})
-Command:addAlias("!armor", "!shield")
+Command:addAlias("!armor", "!shield", "!suit")
 
 Command.Callback = function(Sender, Alias, Targets, Amount)
 	local Targets = Targets or {Sender}
