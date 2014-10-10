5f188e495b3ed58ab5c0470e9a8ca56c46420c59
Nick Lock <EliteJynx@yahoo.co.uk>
2014-10-10 18:38:00 +0100

    Adds permission lib

--- lua/v-handle/libs/V-PermissionLib/main.lua
+++ lua/v-handle/libs/V-PermissionLib/main.lua
@@ -1,1 +1,34 @@
 _V = _V or {}
+
+_V.PermissionLib = {}
+
+_V.PermissionLib.Permissions = {}
+
+local Registry = debug.getregistry()
+
+function _V.PermissionLib.getPermissions(Container)
+	return _V.DataLib.getData(Container, "Permissions")
+end
+
+function _V.PermissionLib.hasPermission(Container, Key)
+	local Perms = V.PermissionLib.getPermissions(Container)
+	return Perms[Key] == true
+end
+
+function Registry.Player:hasPermission(Key)
+	return _V.PermissionLib.hasPermission(self:SteamID(), Key)
+end
+
+if SERVER then
+	
+	function _V.PermissionLib.setPermission(Container, Key, State)
+		local Perms = _V.PermissionLib.getPermissions(Container)
+		Perms[Key] = State
+		_V.DataLib.setData(Container, "Permissions", Perms)
+	end
+
+	function Registry.Player:setPermission(Key, State)
+		_V.PermissionLib.setPermission(self:SteamID(), Key, State)
+	end
+	
+end
