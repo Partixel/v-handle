75be18576a04ff6677ac4c042b4582cec06e4d1b
Nick Lock <EliteJynx@yahoo.co.uk>
2014-10-10 20:13:09 +0100

    Updates

--- lua/autorun/v-handle.lua
+++ lua/autorun/v-handle.lua
@@ -2,21 +2,10 @@ vh = vh or {}
 
 if (SERVER) then
 	include("v-handle/libs/V-FileLib/main.lua")
-	include("v-handle/sv_main.lua")
-	include("v-handle/sh_main.lua")
 	AddCSLuaFile("v-handle/libs/V-FileLib/main.lua")
+	include("v-handle/sh_main.lua")
 	AddCSLuaFile("v-handle/sh_main.lua")
-	AddCSLuaFile("v-handle/cl_main.lua")
-	resource.AddFile("materials/vhandle/adverts.png")
-	resource.AddFile("materials/vhandle/bans.png")
-	resource.AddFile("materials/vhandle/commands.png")
-	resource.AddFile("materials/vhandle/logs.png")
-	resource.AddFile("materials/vhandle/ranks.png")
-	resource.AddFile("materials/vhandle/reports.png")
-	resource.AddFile("materials/vhandle/settings.png")
-	resource.AddFile("materials/vhandle/warnings.png")
 else
 	include("v-handle/libs/V-FileLib/main.lua")
 	include("v-handle/sh_main.lua")
-	include("v-handle/cl_main.lua")
 end
