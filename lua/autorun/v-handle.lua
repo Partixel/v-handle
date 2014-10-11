5e24c32f638e01e7e14ebc90f1b92ccef43b2b16
Nick Lock <EliteJynx@yahoo.co.uk>
2014-10-11 12:28:43 +0100

    Fixes

--- lua/autorun/v-handle.lua
+++ lua/autorun/v-handle.lua
@@ -1,11 +1,11 @@
 vh = vh or {}
 
 if (SERVER) then
-	include("v-handle/libs/v-filelib.lua")
-	AddCSLuaFile("v-handle/libs/v-filelib.lua")
+	include("v-handle/_v-libs/v-filelib.lua")
+	AddCSLuaFile("v-handle/_v-libs/v-filelib.lua")
 	include("v-handle/sh_main.lua")
 	AddCSLuaFile("v-handle/sh_main.lua")
 else
-	include("v-handle/libs/V-filelib.lua")
+	include("v-handle/_v-libs/v-filelib.lua")
 	include("v-handle/sh_main.lua")
 end
