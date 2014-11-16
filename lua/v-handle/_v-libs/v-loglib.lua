87e4ba54c2c4eacc530cc848868e143b60d7b9ca
Partixel <EliteJynx@yahoo.co.uk>
2014-11-16 21:40:03 +0000

    Update v-loglib.lua

--- lua/v-handle/_v-libs/v-loglib.lua
+++ lua/v-handle/_v-libs/v-loglib.lua
@@ -8,12 +8,12 @@ V-LogLib.Type = {
 	CONFIG = 4, -- Used to supply static config variables
 }
 
-LogLib.LogLocation = LogLib.LogLocation or "vh/Logs" -- The directory the file is located in
-LogLib.LogFile = LogLib.LogFile or "log" -- The name of the file - Date + .txt are appended on after this
-LogLib.MessageLevel = LogLib.MessageLevel or 4 -- Any log type lower or equal to this will be printed to console
-LogLib.LogLevel = LogLib.LogLevel or 1 -- Any log type between or equal to this and the MessageLevel will be written to file
+V-LogLib.LogLocation = V-LogLib.LogLocation or "vh/Logs" -- The directory the file is located in
+V-LogLib.LogFile = V-LogLib.LogFile or "log" -- The name of the file - Date + .txt are appended on after this
+V-LogLib.MessageLevel = V-LogLib.MessageLevel or 4 -- Any log type lower or equal to this will be printed to console
+V-LogLib.LogLevel = V-LogLib.LogLevel or 1 -- Any log type between or equal to this and the MessageLevel will be written to file
 
-file.CreateDir(LogLib.LogLocation) -- Make sure the directories exist and initializes LogLocation
+file.CreateDir(V-LogLib.LogLocation) -- Make sure the directories exist and initializes LogLocation
 
 function V-LogLib.Log(Message, Type)
 	if type(Message) == "table" then
@@ -35,19 +35,19 @@ function V-LogLib.Log(Message, Type)
 			return
 		end
 		-- Formates the messages depending on the type
-		local MessageLevel = LogLib.MessageLevel
+		local MessageLevel = V-LogLib.MessageLevel
 		if Type <= MessageLevel then
 			-- Checks it the message should be printed to console
 			print(Message) -- Print the message to the console
 		end
 		
-		if LogLib.LogLevel <= Type and Type <= MessageLevel then
+		if V-LogLib.LogLevel <= Type and Type <= MessageLevel then
 			-- Checks if the message should be logged to file
-			local CurrentFile = tostring(os.date("%d.%m.%y-" .. LogLib.LogFile .. ".txt"))
+			local CurrentFile = tostring(os.date("%d.%m.%y-" .. V-LogLib.LogFile .. ".txt"))
 			-- Creates/finds the file for the current day
 			Message = os.date("%X ") .. string.Explode(" ", os.date("%Z"))[1] .. " - " .. Message
 			-- Formats time and time zone into the message for log purposes
-			file.Append(LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
+			file.Append(V-LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
 			-- Add the message to the log file
 		end
 	else
