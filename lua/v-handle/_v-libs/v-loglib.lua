5deff99c311940960ace5c4b80173c5c79fad029
Partixel <EliteJynx@yahoo.co.uk>
2014-11-16 21:37:38 +0000

    Update v-loglib.lua

--- lua/v-handle/_v-libs/v-loglib.lua
+++ lua/v-handle/_v-libs/v-loglib.lua
@@ -1,8 +1,6 @@
-_V = _V or {}
+V-LogLib = {}
 
-_V.LogLib = {}
-
-_V.LogLib.Type = {
+V-LogLib.Type = {
 	INFO = 0, -- Used for informational messages
 	SEVERE = 1, -- Used to indicate a serious failure
 	WARNING = 2, -- Used to indicate a potential problem
@@ -10,45 +8,50 @@ _V.LogLib.Type = {
 	CONFIG = 4, -- Used to supply static config variables
 }
 
-file.CreateDir(_V.Config.LogLib.LogLocation) -- Make sure the directories exist and initializes LogLocation
+LogLib.LogLocation = LogLib.LogLocation or "vh/Logs" -- The directory the file is located in
+LogLib.LogFile = LogLib.LogFile or "log" -- The name of the file - Date + .txt are appended on after this
+LogLib.MessageLevel = LogLib.MessageLevel or 4 -- Any log type lower or equal to this will be printed to console
+LogLib.LogLevel = LogLib.LogLevel or 1 -- Any log type between or equal to this and the MessageLevel will be written to file
 
-function _V.LogLib.Log(Message, Type)
+file.CreateDir(LogLib.LogLocation) -- Make sure the directories exist and initializes LogLocation
+
+function V-LogLib.Log(Message, Type)
 	if type(Message) == "table" then
 		
 	elseif Message != nil then
 		Message = tostring(Message)
-		if Type == _V.LogLib.Type.INFO then
+		if Type == V-LogLib.Type.INFO then
 			Message = "V-Handle - " .. Message
-		elseif Type == _V.LogLib.Type.WARNING then
+		elseif Type == V-LogLib.Type.WARNING then
 			Message = "V-Handle - WARNING - " .. Message
-		elseif Type == _V.LogLib.Type.SEVERE then
+		elseif Type == V-LogLib.Type.SEVERE then
 			Message = "V-Handle - ERROR - " .. Message
-		elseif Type == _V.LogLib.Type.DEBUG then
+		elseif Type == V-LogLib.Type.DEBUG then
 			Message = "V-Handle - DEBUG - " .. Message
-		elseif Type == _V.LogLib.Type.CONFIG then
+		elseif Type == V-LogLib.Type.CONFIG then
 			Message = "V-Handle - CONFIG - " .. Message
 		else
-			_V.LogLib.Log("Invalid log type type: ".. Message, _V.LogLib.Type.SEVERE)
+			V-LogLib.Log("Invalid log type type: ".. Message, V-LogLib.Type.SEVERE)
 			return
 		end
 		-- Formates the messages depending on the type
-		local MessageLevel = _V.Config.LogLib.MessageLevel
+		local MessageLevel = LogLib.MessageLevel
 		if Type <= MessageLevel then
 			-- Checks it the message should be printed to console
 			print(Message) -- Print the message to the console
 		end
 		
-		if _V.Config.LogLib.LogLevel <= Type and Type <= MessageLevel then
+		if LogLib.LogLevel <= Type and Type <= MessageLevel then
 			-- Checks if the message should be logged to file
-			local CurrentFile = tostring(os.date("%d.%m.%y-" .. _V.Config.LogLib.LogFile .. ".txt"))
+			local CurrentFile = tostring(os.date("%d.%m.%y-" .. LogLib.LogFile .. ".txt"))
 			-- Creates/finds the file for the current day
 			Message = os.date("%X ") .. string.Explode(" ", os.date("%Z"))[1] .. " - " .. Message
 			-- Formats time and time zone into the message for log purposes
-			file.Append(_V.Config.LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
+			file.Append(LogLib.LogLocation .. "/" .. CurrentFile, Message .. "\n")
 			-- Add the message to the log file
 		end
 	else
-		_V.LogLib.Log("Invalid message type: ".. type(Message), _V.LogLib.Type.SEVERE)
+		V-LogLib.Log("Invalid message type: ".. type(Message), V-LogLib.Type.SEVERE)
 		return
 	end
 end
