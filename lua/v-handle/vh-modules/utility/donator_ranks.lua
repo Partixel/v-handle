89ef36ab6bdd2d1e64e7a44efce18a3277a06590
Nick Lock <EliteJynx@yahoo.co.uk>
2014-11-17 16:39:56 +0000

    Donator_Ranks fix

--- lua/v-handle/vh-modules/utility/donator_ranks.lua
+++ lua/v-handle/vh-modules/utility/donator_ranks.lua
@@ -1,9 +1,9 @@
 local DonatorRanks = {
-	Diamond = 20,
-	Gold = 15,
-	Silver = 10,
-	Bronze = 5,
-	Copper = 1
+	{Name = "Diamond", Amount = 20},
+	{Name = "Gold", Amount = 15},
+	{Name = "Silver", Amount = 10},
+	{Name = "Bronze", Amount = 5},
+	{Name = "Copper", Amount = 1},
 }
 
 Registry = debug.getregistry()
@@ -12,20 +12,20 @@ function Registry.Player:getDonatorRank()
 	local Amount = self:getDonatorAmount()
 	if Amount == 0 then return "None" end
 	for a, b in pairs(DonatorRanks) do
-		if Amount >= b then
-			return a
+		if Amount >= b.Amount then
+			return b.Name
 		end
 	end
 end
 
 function Registry.Player:getDonatorAmount()
-	return self:getPlayerData("vh_donation_amount") or 0
+	return self:getPlayerData("DonationAmount") or 0
 end
 
 function Registry.Player:setDonationAmount(Amount)
-	self:setPlayerData("vh_donation_amount", Amount)
+	self:setPlayerData("DonationAmount", Amount)
 end
 
 function Registry.Player:addDonationAmount(Amount)
-	self:setPlayerData("vh_donation_amount", self:getDonatorAmount() + Amount)
+	self:setPlayerData("DonationAmount", self:getDonatorAmount() + Amount)
 end
