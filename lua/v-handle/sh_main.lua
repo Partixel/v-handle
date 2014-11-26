83ba667bb4a70d5814fbfb01083827f2f83ee1a7
Nick Lock <EliteJynx@yahoo.co.uk>
2014-11-26 20:36:12 +0000

    Add config system
    Default configs at v-handle/vh-config
    Normal configs at DATA/v-handle/config

--- lua/v-handle/sh_main.lua
+++ lua/v-handle/sh_main.lua
@@ -1,8 +1,28 @@
 --- Add resources --
 VH_FileLib.AddResourceDir("materials/v-handle/")
 
--- Load configs --
+-- Load def configs --
 VH_FileLib.IncludeDir("vh-config")
+
+-- Load configs --
+function VH_LoadConfigs()
+	local Files, Dirs = file.Find("v-handle/vh-config/*.lua", "LUA")
+	file.CreateDir("v-handle/config")
+	
+	for a, b in ipairs(Files) do
+		local Data = file.Read("v-handle/config/" .. b .. ".txt", "DATA")
+		if Data and Data != "" then
+			RunStringEx(Data, "VH-Configs")
+		else
+			Data = file.Read("v-handle/vh-config/" .. b, "LUA")
+			if Data and Data != "" then
+				file.Write("v-handle/config/" .. b .. ".txt", Data)
+			end
+		end
+	end
+end
+
+VH_LoadConfigs()
 
 -- Load libs --
 VH_FileLib.IncludeDir("vh-libs")
